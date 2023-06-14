d92d3f714cf12a1611f3b1a17a940eaadf61b93f
Claudio Valerio <claudio.valerio@zero.it>
2023-06-14 15:40:08 +0200

    events on video

--- wwwroot/js/video.js
+++ wwwroot/js/video.js
@@ -1,17 +1,42 @@
 const video = {
   playLocal: function (elementId) {
-    document.getElementById(elementId).src = "videos/mov_bbb.mp4";
+    const videoEl = document.getElementById(elementId);
+    if (/^blob/.test(videoEl.src)) {
+      URL.revokeObjectURL(videoEl.src);
+    }
+    videoEl.src = "videos/mov_bbb.mp4";
+  },
 
+  playLocalWithPrefetch: async function (elementId) {
+    const blob = await fetch("videos/mov_bbb.mp4").then(r => r.blob());
+    const url = URL.createObjectURL(blob);
+    document.getElementById(elementId).src = url;
   },
 
   playRemote: function (elementId) {
-    document.getElementById(elementId).src = "https://www.w3schools.com/html/mov_bbb.mp4";
-
+    const videoEl = document.getElementById(elementId);
+    if (/^blob/.test(videoEl.src)) {
+      URL.revokeObjectURL(videoEl.src);
+    }
+    videoEl.src = "https://www.w3schools.com/html/mov_bbb.mp4";
   },
 
   init: function (elementId) {
-    document.getElementById(elementId).addEventListener('error', (err) => {
+    const videoEl = document.getElementById(elementId);
+    videoEl.addEventListener('error', (err) => {
       console.error(err.target.error.message);
+    });
+    videoEl.addEventListener('loadedmetadata', (e) => {
+      console.log('loaded video metadata', e.target.duration);
+    });
+    videoEl.addEventListener('loadeddata', (e) => {
+      console.log('loaded video data', e.target.duration);
+    });
+    videoEl.addEventListener('playing', (e) => {
+      console.log('playing video', e.target.duration);
+    });
+    videoEl.addEventListener('pause', (e) => {
+      console.log('pause video', e.target.duration);
     });
   }
 }
